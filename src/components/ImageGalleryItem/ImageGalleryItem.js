ebf1869f2101a555704f406365bb92c7d9b1b185
ZEugenii <zinich911@gmail.com>
2022-12-18 13:25:47 +0200

    refactoring ImageGalleryItem on hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,37 +1,63 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Modal from 'components/Modal/Modal';
 import { GalleryItem, ImgGallery } from './ImageGalleryItem.style';
 
-class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
-    modalImg: '',
+export default function ImageGalleryItem(gallery) {
+  const [showModal, setShowModal] = useState(false);
+
+  const { id, webformatURL, largeImageURL, tag } = gallery;
+
+  const toggleModal = () => {
+    setShowModal(prevState => !prevState);
   };
 
-  toggleModal = e => {
-    this.setState({
-      showModal: !this.state.showModal,
-    });
-  };
-
-  render() {
-    const { id, webformatURL, largeImageURL, tag } = this.props;
-    return (
-      <>
-        <GalleryItem key={id}>
-          <ImgGallery
-            id={id}
-            src={webformatURL}
-            alt={tag}
-            onClick={this.toggleModal}
-          />
-        </GalleryItem>
-        {this.state.showModal && (
-          <Modal modalImg={largeImageURL} toggleModal={this.toggleModal} />
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <GalleryItem key={id}>
+        <ImgGallery
+          id={id}
+          src={webformatURL}
+          alt={tag}
+          onClick={toggleModal}
+        />
+      </GalleryItem>
+      {showModal && (
+        <Modal modalImg={largeImageURL} toggleModal={toggleModal} />
+      )}
+    </>
+  );
 }
 
-export default ImageGalleryItem;
+// class ImageGalleryItem extends Component {
+//   state = {
+//     showModal: false,
+//     modalImg: '',
+//   };
+
+//   toggleModal = () => {
+//     this.setState({
+//       showModal: !this.state.showModal,
+//     });
+//   };
+
+//   render() {
+//     const { id, webformatURL, largeImageURL, tag } = this.props;
+// return (
+//   <>
+//     <GalleryItem key={id}>
+//       <ImgGallery
+//         id={id}
+//         src={webformatURL}
+//         alt={tag}
+//         onClick={this.toggleModal}
+//       />
+//     </GalleryItem>
+//     {this.state.showModal && (
+//       <Modal modalImg={largeImageURL} toggleModal={this.toggleModal} />
+//     )}
+//   </>
+// );
+//   }
+// }
+
+// export default ImageGalleryItem;
